2039c189030d121d5678e37bef7175e2bd9c5417
pawel <71666955+khizhnyakov@users.noreply.github.com>
2020-12-15 19:07:14 +0300

    task 1.1/3

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -23,7 +23,10 @@ function sayHello(userName) {
 
 function calculateFormula() {
     
-	// реализуйте здесь задание №3
+let x=2;
+let y=22;
+let z=0;
+let result=(x * y + 5 * z + x - 1);
     
-    return // ваш ответ;
+    return (result)
 }
