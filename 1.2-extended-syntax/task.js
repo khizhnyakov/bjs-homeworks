7f36f97308f03c79d7f0d3f86a45b70f635f0e03
pawel <71666955+khizhnyakov@users.noreply.github.com>
2020-12-23 21:21:50 +0300

    add 1.2-task 1

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,7 +1,20 @@
+"use strict"
+
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x =[];
+    let d =(b**2-4*a*c);
+    if (d===0) {
+     x[0] = -b/2*a;
+    }
+    else if (d<0) {
+    }
+    else {
+        x[0]=(- b + Math.sqrt(d)) / (2 * a);
+        x[1]=(- b - Math.sqrt(d)) / (2 * a); 
+    }
+    return x;
 }
+console.log (getResult(2,4,-3))
 
 function getAverageMark(marks){
     // код для задачи №2 писать здесь
