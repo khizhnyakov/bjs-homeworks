7202ae47750ab3ed0caeb0d4c56246ef6ec56ee7
pawel <71666955+khizhnyakov@users.noreply.github.com>
2021-01-03 16:08:31 +0300

    add task 1.3/1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,22 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    let monthDateNow = new Date().getMonth();
+    let yearNow = new Date().getFullYear();
+    let monthDeadLine = date.getMonth();
+    let yearDedline = date.getFullYear();
+    const amountMonth = (+yearDedline - +yearNow) * 12 - (+monthDateNow) + date.getMonth();
+    let percentMonth = +percent / 12;
+    let credit = +amount - +contribution;
+    let payMouth = (+credit * percentMonth)/ Math.pow((1 -(1 + percentMonth)), -36);
+    let totalAmount = +payMouth * +amountMonth + +credit;
+    return totalAmount.toFixed(2) ;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     // return greeting;
 }
+
+
+
+
