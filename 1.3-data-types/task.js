1eef88ad537f0a8984edd5565d2af9d3d4c48b56
pawel <71666955+khizhnyakov@users.noreply.github.com>
2021-01-03 20:28:49 +0300

    edit task 1.3/1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
     let monthDateNow = new Date().getMonth();
@@ -5,12 +7,15 @@ function calculateTotalMortgage(percent, contribution, amount, date) {
     let monthDeadLine = date.getMonth();
     let yearDedline = date.getFullYear();
     const amountMonth = (+yearDedline - +yearNow) * 12 - (+monthDateNow) + date.getMonth();
-    let percentMonth = +percent / 12;
+    let percentMonth = +percent / 100 / 12;
     let credit = +amount - +contribution;
-    let payMouth = (+credit * percentMonth)/ Math.pow((1 -(1 + percentMonth)), -36);
+    let payMouth = credit * (percentMonth+percentMonth/(Math.pow((1 + percentMonth), amountMonth)-1));
     let totalAmount = +payMouth * +amountMonth + +credit;
-    return totalAmount.toFixed(2) ;
+    {console.log (`Всего с процентами: ${totalAmount.toFixed(2)} рублей`)}
+    return (`Всего с процентами: ${totalAmount.toFixed(2)} рублей`)
 }
+
+
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
