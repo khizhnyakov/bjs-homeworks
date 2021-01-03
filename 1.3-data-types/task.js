ae6200f7ee20955581c8165d0ec1e877e09dd7d3
pawel <71666955+khizhnyakov@users.noreply.github.com>
2021-01-03 20:36:27 +0300

    добавил комментарии к задчае 1.3/1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,20 +1,18 @@
 "use strict"
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    let monthDateNow = new Date().getMonth();
-    let yearNow = new Date().getFullYear();
-    let monthDeadLine = date.getMonth();
-    let yearDedline = date.getFullYear();
-    const amountMonth = (+yearDedline - +yearNow) * 12 - (+monthDateNow) + date.getMonth();
-    let percentMonth = +percent / 100 / 12;
-    let credit = +amount - +contribution;
-    let payMouth = credit * (percentMonth+percentMonth/(Math.pow((1 + percentMonth), amountMonth)-1));
-    let totalAmount = +payMouth * +amountMonth + +credit;
+function calculateTotalMortgage(percent, contribution, amount, date){
+    let monthDateNow = new Date().getMonth(); //смторим месяц даты текущей
+    let yearNow = new Date().getFullYear(); //смторим год даты текущей
+    let monthDeadLine = date.getMonth(); //смторим месяц даты окончания кредита по вводу через форму
+    let yearDedline = date.getFullYear(); //смторим год  даты окончания кредита по вводу через форму
+    const amountMonth = (+yearDedline - +yearNow) * 12 - (+monthDateNow) + date.getMonth(); //считаем колличество месяцев в кредите
+    let percentMonth = +percent / 100 / 12; // считаем процент в месяц
+    let credit = +amount - +contribution; // считаем тело кредита
+    let payMouth = credit * (percentMonth+percentMonth/(Math.pow((1 + percentMonth), amountMonth)-1)); // считаеем процент за время кредита 
+    let totalAmount = +payMouth * +amountMonth + +credit; // считаем все расходы клиента 
     {console.log (`Всего с процентами: ${totalAmount.toFixed(2)} рублей`)}
     return (`Всего с процентами: ${totalAmount.toFixed(2)} рублей`)
 }
-
 
 
 function getGreeting(name) {
