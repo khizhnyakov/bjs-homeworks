58d7c131bafdc0d4c2aed1594c7c2f0e8b61790f
pawel <71666955+khizhnyakov@users.noreply.github.com>
2021-01-05 17:21:45 +0300

    оптимизация задачи 1.3/1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,11 +1,7 @@
 "use strict"
 
 function calculateTotalMortgage(percent, contribution, amount, date){
-    let monthDateNow = new Date().getMonth(); //смотрим месяц даты текущей
-    let yearNow = new Date().getFullYear(); //смотрим год даты текущей
-    let monthDeadLine = date.getMonth(); //смторим месяц даты окончания кредита по вводу через форму
-    let yearDedline = date.getFullYear(); //смотрим год  даты окончания кредита по вводу через форму
-    const amountMonth = (+yearDedline - +yearNow) * 12 - (+monthDateNow) + date.getMonth(); //считаем колличество месяцев в кредите
+    const amountMonth = (date.getFullYear() - new Date().getFullYear()) * 12 - (+new Date().getMonth()) + date.getMonth(); //считаем колличество месяцев в кредите
     let percentMonth = +percent / 100 / 12; // считаем процент в месяц
     let credit = +amount - +contribution; // считаем тело кредита
     let payMouth = credit * (percentMonth+percentMonth/(Math.pow((1 + percentMonth), amountMonth)-1)); // считаеем тело кредита+проценты в месяц
