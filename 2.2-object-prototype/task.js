d769a93e435ff1a9d40e55777581520ffc88797a
pawel <71666955+khizhnyakov@users.noreply.github.com>
2021-01-20 21:20:38 +0300

    task 2.2/1

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,4 +1,16 @@
-//String.prototype.isPalindrome - для задачи №1
+
+String.prototype.isPalindrome = function (){
+  char  = this.toString().toLowerCase().split(' ').join('').split(''); // разбивает строку на список символов
+  return (char.join() == char.reverse().join())
+   //  возвращает true если условие в скобках выполняется, если список букв читается одинаково и в прямом и в обратном порядке
+ }
+ 
+   //  возвращает true если условие в скобках выполняется, если список букв читается одинаково и в прямом и в обратном порядке
+
+/*String.prototype.isPalindrome = function () {
+var normalized = this.toLowerCase().match(/[a-z]/gi).reverse();
+return normalized.join('') === normalized.reverse().join('');
+}*/
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
@@ -10,4 +22,3 @@ function checkBirthday(birthday) {
     // return verdict
 }
 
- 
